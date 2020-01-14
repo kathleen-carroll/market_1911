b86ddd489aca0ddc533d6d83da9609e26ebd0080
Kathleen Carroll <khakicarroll@comcast.net>
2020-01-14 10:57:47 -0700

    Finish iteration 4

--- lib/market.rb
+++ lib/market.rb
@@ -33,4 +33,27 @@ class Market
     end
     item_list
   end
+
+  def sell(item, amount)
+    item_qty = Hash.new(0)
+    @vendors.map do |vendor|
+      vendor.inventory.map{|item, quantity| item_qty[item] = quantity}
+    end
+
+    boolean = item_qty.any? {|item_val, quantity| quantity >= amount && item_val == item}
+    return boolean if boolean == false
+
+    remainder = 0
+    vendors_that_sell(item).each do |vendor|
+      if vendor.inventory[item] < amount
+        remainder = amount - vendor.inventory[item]
+        vendor.inventory[item] = 0
+      elsif remainder > 0
+        vendor.inventory[item] -= remainder
+      else
+        vendor.inventory[item] -= amount
+      end
+    end
+    boolean
+  end
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -69,6 +69,15 @@ class VendorTest < Minitest::Test
   end
 
   def test_it_can_sell_products_and_reduce_stock
-    skip
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    assert_equal false, @market.sell(@item1, 200)
+    assert_equal false, @market.sell(@item5, 1)
+    assert_equal true, @market.sell(@item4, 5)
+    assert_equal 45, @vendor2.check_stock(@item4)
+    assert_equal true, @market.sell(@item1, 40)
+    assert_equal 0, @vendor1.check_stock(@item1)
+    assert_equal 60, @vendor3.check_stock(@item1)
   end
 end
